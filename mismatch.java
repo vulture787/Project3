aa75132bec0649395b9dda306a3bdaa53aec464e
vulture787 <91385513+vulture787@users.noreply.github.com>
2021-10-30 11:27:23 +0530

    Create mismatch.java

--- mismatch.java
+++ mismatch.java
@@ -8,3 +8,21 @@ public class mismatch {
         int nums[] = findErrorNums(arr);
         System.out.println(Arrays.toString(nums));
     }
+ public static int[] findErrorNums(int[] arr) {
+        int nums[] = new int[2];
+        int i = 0;
+        while (i < arr.length) {
+
+            int search = arr[i] - 1;
+            if (arr[i] != arr[search]) {
+                nums[0] = search + 1;
+                nums[1] = 0;
+
+            } else
+                i++;
+
+
+        }
+        return nums;
+    }
+}
